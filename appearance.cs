78181afd863c4f99a230823fdbe33573ae64a4e3
element670 <144697828+element670@users.noreply.github.com>
2024-05-13 23:56:21 +0400

    Create appearance.cs

---
+++ appearance.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "Character", menuName = "Character/Appearance")]
+public class CharacterAppearance : ScriptableObject
+{
+    public Sprite eyes;
+
+    public Sprite mouths;
+
+    public Sprite weapon;
+    public string Charactername;
+}
